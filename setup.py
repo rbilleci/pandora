5781c9ee1afaced260f1b623462bd3be5368c409
billecr <billecr@amazon.com>
2021-02-07 15:56:14 +0100

    add setup.py, update libs

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pandora',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Richard Billeci',
+    author_email='rick.billeci@gmail.com',
+    description=''
+)
